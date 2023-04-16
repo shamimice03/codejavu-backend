a332fba6930bc4554700035ce8efe39962d2690a
rummanrc <rumman179@gmail.com>
2023-04-16 17:55:21 +0900

    column type error fix

--- backend/src/app/models/snippet.py
+++ backend/src/app/models/snippet.py
@@ -17,7 +17,7 @@ class Snippet(Base):
     id: int = Column(Integer, primary_key=True, index=True)
     title: str = Column(String, index=True)
     snippet: str = Column(String, index=True)
-    user_id: int = Column(Integer, ForeignKey("user.id"))
+    user_id: Column["Integer"] = Column(Integer, ForeignKey("user.id"))
     user: "User" = relationship("User", back_populates="snippets")
     language_id: int = Column(Integer, ForeignKey("language.id"))
     language: "Language" = relationship("Language", back_populates="snippets")
